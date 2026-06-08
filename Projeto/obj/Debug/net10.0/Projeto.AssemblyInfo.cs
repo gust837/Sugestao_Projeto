afe8feaa9c4a014f070f8a9533d456127eee95a2
GUSTAVO GABRIEL DOS SANTOS PEDRO <52663088854@info.local>
2026-06-08 11:30:00 -0300

    Criando pastas

--- Projeto/obj/Debug/net10.0/Projeto.AssemblyInfo.cs
+++ Projeto/obj/Debug/net10.0/Projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f69888c0f1eded69a0e0451518b21add4ac2298")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
