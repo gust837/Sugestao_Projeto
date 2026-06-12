0a64f809565e56c4809f29eea34a06ade344a858
BRUNO TAVARES PELLEGRINO <54166966804@info.local>
2026-06-12 10:52:02 -0300

    .gitignore implementado

--- Projeto/obj/Debug/net10.0/Projeto.AssemblyInfo.cs
+++ Projeto/obj/Debug/net10.0/Projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a865bd5cdcbb0421c46a55dd8c06697defc6e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32b30e60a394ce6a562dce1894f134fc11033a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
