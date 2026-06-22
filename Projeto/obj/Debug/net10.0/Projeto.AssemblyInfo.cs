e6d3aa4235cd4c4ee3aa7acf9833033d2fae16d8
FIC <fic@info.local>
2026-06-22 08:30:15 -0300

    API gemini feito 2.0

--- Projeto/obj/Debug/net10.0/Projeto.AssemblyInfo.cs
+++ Projeto/obj/Debug/net10.0/Projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae56a498615c014bc5cc7eee803495cf7f2edbc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65a871c44b7e8efc2f51814b35f2b22ba44176f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
